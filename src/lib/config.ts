0dbf2837216298666d6cb172f0f54cc022bc8215
Artemy Egorov <artegoser@gmail.com>
2025-07-09 20:55:59 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,8 @@
 export const image_base_url = 'https://image.pollinations.ai/prompt/';
 
-export const text_base_url = 'https://text.pollinations.ai/openai?token=2h4sDXfpwYNtvZjc';
+export const text_base_url = 'https://text.pollinations.ai/openai';
 export const text_model = 'openai';
 
-export const audio_base_url = 'https://text.pollinations.ai/openai?token=2h4sDXfpwYNtvZjc';
+export const audio_base_url = 'https://text.pollinations.ai/openai';
 export const autoplay_audio = true;
 export const audio_voice = 'ash';
